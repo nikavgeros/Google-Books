c021234e52ce504c36e2d619fdcf4882913b1423
Nikos Avgeros <76570431+nikavgeros@users.noreply.github.com>
2023-03-20 21:26:31 +0200

    Update Utils.java

--- Google-Books-API/src/main/java/org/utils/Utils.java
+++ Google-Books-API/src/main/java/org/utils/Utils.java
@@ -2,5 +2,5 @@ package org.utils;
 
 public class Utils {
 
-    public static final String API_KEY = "AIzaSyCb37e-LnPC3BMc6SZUXOOdjFwJ_fK_OI0";
+    public static final String API_KEY = "YOUR API KEY";
 }
